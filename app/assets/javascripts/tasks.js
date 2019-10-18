72bd98be268468db43481d238a936f5476cead49
Wes Griffin <typicalgames@gmail.com>
2019-10-18 03:09:28 +0000

    finish

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,7 +4,9 @@ $(function() {
     // of the task and produces an HTML representation using <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'"class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -27,7 +29,12 @@ $(function() {
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+      } );
     }
 
 
